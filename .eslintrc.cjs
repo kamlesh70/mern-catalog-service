445cb8aaecc4c4fc1024778161ff52317741379f
kamlesh70 <kamalmehra3571@gmail.com>
2024-05-06 08:55:03 +0530

    add s3 file upload

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
     "@typescript-eslint/require-await": "off",
     "@typescript-eslint/unbound-method": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
-    "@typescript-eslint/no-explicit-any": "off"
+    "@typescript-eslint/no-explicit-any": "off",
   },
   root: true,
 };
